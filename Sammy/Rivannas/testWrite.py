2ff8aae4e2c87e7436c94fdecb24ac4448f77a8a
SammyHL <sammyhl3027@gmail.com>
2021-04-30 15:09:19 -0400

    Refactoring/Organizing Rivanna Results

--- Sammy/Rivannas/testWrite.py
+++
@@ -1,6 +0,0 @@
-learning_rate = 0.001
-activationFunction = "ReLU"
-f = open("/home/sl8rn/test.txt", "x")
-f.write("Learning Rate: " + str(learning_rate)+ "\n")
-f.write("ActivationFunction: " + activationFunction)
-f.close()
