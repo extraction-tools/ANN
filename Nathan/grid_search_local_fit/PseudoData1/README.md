285c1dc524d2e6371e43e1ef7d30aa3b6e04a705
Nathan Snyder <nathan.snyder13@gmail.com>
2021-06-05 09:13:27 -0700

    made miscellaneous improvements to grid search local fit

--- Nathan/grid_search_local_fit/PseudoData1/README.md
+++
@@ -1,7 +0,0 @@
-# Grid Search Local Fit
-
-To compile, run ```make```
-
-To perform the grid search local fit on all sets, run ```make run```
-
-To perform the grid search local fit on one set, run ```make run set=?```.
--- Nathan/grid_search_local_fit/grid_search_local_fit.c
+++ Nathan/grid_search_local_fit/grid_search_local_fit.c
@@ -14,34 +14,30 @@ typedef struct {
     double ReH, ReE, ReHtilde, error;
 } CFFGuess;
 
-double k, QQ, x_b, t,
-       F[NUM_INDEXES], errF[NUM_INDEXES],
-       F1, F2,
-       dvcs,
-       ReH_guess, ReE_guess, ReHtilde_guess,
-       ReH_mean, ReE_mean, ReHtilde_mean,
-       ReH_stddev, ReE_stddev, ReHtilde_stddev;
+static double k, QQ, x, t,
+              F[NUM_INDEXES], errF[NUM_INDEXES],
+              F1, F2,
+              dvcs,
+              ReH_guess, ReE_guess, ReHtilde_guess,
+              ReH_mean, ReE_mean, ReHtilde_mean,
+              ReH_stddev, ReE_stddev, ReHtilde_stddev;
 
-unsigned int phi[NUM_INDEXES];
+static int desiredSet, phi[NUM_INDEXES];
 
-TVA1_UU tva1_uu;
-
-CFFGuess cffGuesses[NUM_REPLICAS];
-
-unsigned int desiredSet;
+static CFFGuess cffGuesses[NUM_REPLICAS];
 
 // Read in the data
 // Returns 0 if success, 1 if error
 bool readInData(char *filename);
 
 // Box-Muller Transform --> Turn a uniform distribution into a Gaussian distribution
-double boxMuller();
+double boxMuller(void);
 
 // Function that calculates the mean and standard deviation of the CFFs over all the replicas
 void calcMeanAndStdDev(void);
 
 // Calculate the mean percent error in F for a specific replica and the given CFFs
-double calcFError(double *replicas, double ReH, double ReE, double ReHtilde);
+double calcFError(double replicas[], double ReH, double ReE, double ReHtilde);
 
 // Estimate the correct CFF values for a specific replica
 void calcCFFs(int replicaNum);
@@ -52,42 +48,39 @@ void localFit(void);
 
 
 int main(int argc, char *argv[]) {
-    srand(time(0));
+    srand((unsigned int) time(0));
 
     if (argc == 3) {
         desiredSet = atoi(argv[2]);
-        if(readInData(argv[1]) == 1) return 1;
+        if(readInData(argv[1])) {
+            printf("Read failed.\n");
+            return 1;
+        }
     } else {
-        printf("Please specify a data file (.csv) and a set number only\n");
+        printf("Please specify a data file (.csv) and a set number only.\n");
         return 1;
     }
-/*
-    for (int i = 0; i < NUM_REPLICAS; i += 2) {
-        boxMuller(&cffGuesses[i].replica, &cffGuesses[i+1].replica);
-    }
-*/
 
     localFit();
+
+/*
+    printf("ReH_guess: %lf, ReH_mean: %lf, ReH_stddev: %lf\n", ReH_guess, ReH_mean, ReH_stddev);
+    printf("ReE_guess: %lf, ReE_mean: %lf, ReE_stddev: %lf\n", ReE_guess, ReE_mean, ReE_stddev);
+    printf("ReHtilde_guess: %lf, ReHtilde_mean: %lf, ReHtilde_stddev: %lf\n\n", ReHtilde_guess, ReHtilde_mean, ReHtilde_stddev);
+*/
 
     FILE *f = fopen("grid_search_local_fit_output.csv", "r");
 
     if (f == NULL) {
         f = fopen("grid_search_local_fit_output.csv", "w");
-        fprintf(f, "set,ReH_actual,ReH_mean,ReH_stddev,ReE_actual,ReE_mean,ReE_stddev,ReHtilde_actual,ReHtilde_mean,ReHtilde_stddev\n");
+        fprintf(f, "Set,ReH_mean,ReH_stddev,ReE_mean,ReE_stddev,ReHtilde_mean,ReHtilde_stddev\n");
     } else {
         fclose(f);
         f = fopen("grid_search_local_fit_output.csv", "a");
     }
 
-    calcMeanAndStdDev();
+    fprintf(f, "%d,%lf,%lf,%lf,%lf,%lf,%lf\n", desiredSet, ReH_mean, ReH_stddev, ReE_mean, ReE_stddev, ReHtilde_mean, ReHtilde_stddev);
 
-
-    printf("ReH_mean: %lf, ReH_stddev: %lf\n", ReH_mean, ReH_stddev);
-    printf("ReE_mean: %lf, ReE_stddev: %lf\n", ReE_mean, ReE_stddev);
-    printf("ReHtilde_mean: %lf, ReHtilde_stddev: %lf\n\n", ReHtilde_mean, ReHtilde_stddev);
-
-
-    fprintf(f, "%d,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf\n", desiredSet, ReH_guess, ReH_mean, ReH_stddev, ReE_guess, ReE_mean, ReE_stddev, ReHtilde_guess, ReHtilde_mean, ReHtilde_stddev);
     fclose(f);
 
     return 0;
@@ -99,31 +92,33 @@ int main(int argc, char *argv[]) {
 // returns 0 if success, 1 if error
 bool readInData(char *filename) {
     FILE *f = fopen(filename, "r");
-    char buff[1024];
+    char buff[1024] = {0};
     bool previouslyEncounteredSet = 0;
 
-    if (f == NULL) return 1;
+    if (f == NULL) {
+        printf("Error: could not open file %s.\n", filename);
+        return 1;
+    }
 
     fgets(buff, 1024, f);
 
     while (fgets(buff, 1024, f)) {
-        unsigned int set, index;
+        int set, index;
 
-        sscanf(buff, "%u,%*s\n", &set);
+        sscanf(buff, "%d,%*s\n", &set);
 
         if (set == desiredSet) {
-            sscanf(buff, "%*u,%u,%*s\n", &index);
+            sscanf(buff, "%*d,%d,%*s\n", &index);
 
             if (!previouslyEncounteredSet) {
-                sscanf(buff, "%*u,%*u,%lf,%lf,%lf,%lf,%u,%lf,%lf,%*lf,%lf,%lf,%lf\n",
-                       &k, &QQ, &x_b, &t,
+                sscanf(buff, "%*d,%*d,%lf,%lf,%lf,%lf,%d,%lf,%lf,%*lf,%lf,%lf,%lf\n",
+                       &k, &QQ, &x, &t,
                        &phi[index], &F[index], &errF[index],
-                       &F1, &F2,
-                       &dvcs);
+                       &F1, &F2, &dvcs);
 
                 previouslyEncounteredSet = 1;
             } else {
-                sscanf(buff, "%*u,%*u,%*lf,%*lf,%*lf,%*lf,%u,%lf,%lf,%*lf,%*lf,%*lf,%*lf\n",
+                sscanf(buff, "%*d,%*d,%*lf,%*lf,%*lf,%*lf,%d,%lf,%lf,%*lf,%*lf,%*lf,%*lf\n",
                        &phi[index], &F[index], &errF[index]);
             }
         }
@@ -131,15 +126,20 @@ bool readInData(char *filename) {
 
     fclose(f);
 
-    return !previouslyEncounteredSet;
+    if (!previouslyEncounteredSet) {
+        printf("Error: could not find set %d in %s.\n", desiredSet, filename);
+        return 1;
+    }
+
+    return 0;
 }
 
 
 
 // Box-Muller Transform --> Turn a uniform distribution into a Gaussian distribution
-// Returns a random number following a Gaussian distribution wheree mean=0 and stddev=1
-double boxMuller() {
-    return sqrt(-2. * log((double) rand() / RAND_MAX)) * sin(2. * M_PI * ((double) rand() / RAND_MAX));
+// Returns a random number following a Gaussian distribution with mean=0 and stddev=1
+double boxMuller(void) {
+    return sqrt(-2 * log(rand() / ((double) RAND_MAX))) * sin(2 * M_PI * (rand() / ((double) RAND_MAX)));
 }
 
 
@@ -174,50 +174,57 @@ void calcMeanAndStdDev(void) {
 
 
 // Calculate the mean percent error in F for a specific replica and the given CFFs
-double calcFError(double *replicas, double ReH, double ReE, double ReHtilde) {
-    double mean_percent_error = 0.0;
+double calcFError(double replicas[], double ReH, double ReE, double ReHtilde) {
+    double sum_of_percent_errors = 0.0;
 
     for (int index = 0; index < NUM_INDEXES; index++) {
-        double F_predicted = TVA1_UU_GetBHUU(&tva1_uu, phi[index], F1, F2) +
-                             TVA1_UU_GetIUU(&tva1_uu, phi[index], F1, F2, ReH, ReE, ReHtilde) + 
+        double F_predicted = TVA1_UU_GetBHUU(phi[index], F1, F2) +
+                             TVA1_UU_GetIUU(phi[index], F1, F2, ReH, ReE, ReHtilde) +
                              dvcs;
 
         double F_actual = F[index] + (replicas[index] * errF[index]);
 
-        double error = (F_actual - F_predicted) / F_actual;
+
+        double percent_error = (F_actual - F_predicted) / F_actual;
 
         // Only care about the magnitude of the error, not the sign
-        if (error < 0) error = -1 * error;
+        if (percent_error < 0) percent_error *= -1;
 
-        //printf("Index %d: phi=%u, F_predicted = %lf, F_actual = %lf, Error = %lf\n", index, phi[index], F_predicted, F_actual, error);
-
-        mean_percent_error += error;
+        sum_of_percent_errors += percent_error;
     }
 
-    mean_percent_error /= NUM_INDEXES;
-
-    // Return the mean percent error
-    return mean_percent_error;
+    return sum_of_percent_errors / NUM_INDEXES;
 }
 
 
 
 // Estimate the correct CFF values for a specific replica
 void calcCFFs(int replicaNum) {
-    double ReHguess, ReEguess, ReHtildeguess, bestError;
+    double ReHguess, ReEguess, ReHtildeguess;
 
-    double replicas[] = {(replicaNum == -1) ? 0.0 : boxMuller()};
+    double bestError = DBL_MAX;
 
-    // num: number of points tested on either side of the guess
-    const unsigned int num = 10;
-
-    // totalDist: the total distance being tested on either side of the guess
-    // Value is arbitrary for correctness
-    // 100:10 makes the algorithm faster for the pseudodata
-    const unsigned int totalDist = (replicaNum == -1) ? 100 : 10;
+    // replica: the number of standard deviations that the values of F will be off by
+    double replicas[NUM_INDEXES];
 
     if (replicaNum == -1) {
-        ReHguess = ReEguess = ReHtildeguess = 0;
+        for (int i = 0; i < NUM_INDEXES; i++) replicas[i] = 0.0;
+    } else {
+        for (int i = 0; i < NUM_INDEXES; i++) replicas[i] = boxMuller();
+    }
+
+    // num: number of points tested on either side of the guess
+    // Value is arbitrary for correctness but greatly impacts the speed of the program
+    const int num = 10;
+
+    // totalDist: the total distance being tested on either side of the guess
+    // Value is arbitrary for correctness but greatly impacts the speed of the program
+    const int totalDist = (replicaNum == -1) ? 100 : 10;
+
+    if (replicaNum == -1) {
+        ReHguess = 0.0;
+        ReEguess = 0.0;
+        ReHtildeguess = 0.0;
     } else {
         ReHguess = ReH_guess;
         ReEguess = ReE_guess;
@@ -225,26 +232,20 @@ void calcCFFs(int replicaNum) {
     }
 
     // dist: distance between each point being tested
-    for (double dist = (double) totalDist / (double) num; dist >= 0.001; dist /= num) {
-        //printf("dist = %lf\n", dist);
+    for (double dist = (double) totalDist / (double) num; dist >= 0.0001; dist /= num) {
         double maxChange = dist * num, minChange = -1 * dist * num;
         double bestReHguess = 0, bestReEguess = 0, bestReHtildeguess = 0;
         double prevError = DBL_MAX;
 
         bestError = DBL_MAX;
 
-        while ((prevError != bestError) || 
-               (prevError - bestError >= 0.000001 || prevError - bestError <= -0.000001) || 
-               (bestError == DBL_MAX)) {
-
+        while ((fabs(bestError - prevError) > 0.000001) || (DBL_MAX - bestError < 0.000001)) {
             prevError = bestError;
-
-            //printf("ReHguess = %lf, Best error = %lf\n", ReHguess, bestError);
 
             for (double ReHchange = minChange; ReHchange <= maxChange; ReHchange += dist) {
                 for (double ReEchange = minChange; ReEchange <= maxChange; ReEchange += dist) {
                     for (double ReHtildechange = minChange; ReHtildechange <= maxChange; ReHtildechange += dist) {
-                        double error = calcFError((double *) &replicas, ReHguess + ReHchange, ReEguess + ReEchange, ReHtildeguess + ReHtildechange);
+                        double error = calcFError(replicas, ReHguess + ReHchange, ReEguess + ReEchange, ReHtildeguess + ReHtildechange);
 
                         if (error < bestError) {
                             bestReHguess = ReHguess + ReHchange;
@@ -278,25 +279,18 @@ void calcCFFs(int replicaNum) {
 
 // Fits CFFs to all of the replicas
 void localFit(void) {
-    //double averageMPE_F = 0.0, averageMPE_F_actual = 0.0;
+    TVA1_UU_SetKinematics(QQ, x, t, k);
 
-    TVA1_UU_Init(&tva1_uu);
-    TVA1_UU_SetKinematics(&tva1_uu, QQ, x_b, t, k);
-
-    printf("Starting set %d...\n", desiredSet);
+    //printf("Starting set %d...\n", desiredSet);
 
     calcCFFs(-1);
 
-    printf("\033[ASet %d: 0.00%% complete\n", desiredSet);
+    //printf("\033[ASet %d: 0.00%% complete\n", desiredSet);
 
     for (int replicaNum = 0; replicaNum < NUM_REPLICAS; replicaNum++) {
         calcCFFs(replicaNum);
-/*
-        printf("\n\nReH: %lf\n", cffGuesses[replicaNum].ReH);
-        printf("ReE: %lf\n", cffGuesses[replicaNum].ReE);
-        printf("ReHtilde: %lf\n", cffGuesses[replicaNum].ReHtilde);
-        printf("Error: %lf\n\n\n", cffGuesses[replicaNum].error);
-*/
-        printf("\033[ASet %d: %.2lf%% complete\n", desiredSet, (100. * (double) (replicaNum + 1) / (double) NUM_REPLICAS));
+        //printf("\033[ASet %d: %.2lf%% complete\n", desiredSet, (100. * (double) (replicaNum + 1) / (double) NUM_REPLICAS));
     }
+
+    calcMeanAndStdDev();
 }
