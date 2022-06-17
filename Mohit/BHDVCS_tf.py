88d8d36cac8565e9b4166c36b7baf5887eef1c18
boxrof <68367261+IROATUVA@users.noreply.github.com>
2022-06-17 15:10:53 -0400

    Update BHDVCS_tf.py

--- Mohit/BHDVCS_tf.py
+++ Mohit/BHDVCS_tf.py
@@ -138,7 +138,7 @@ def F2VsPhi(dataframe,SetNum,xdat,cffs,designation="overall"):
   plt.legend(loc=4,fontsize=10,handlelength=3)
   plt.title("Local fit with data set #"+str(SetNum),fontsize=20)
   plt.plot(temp_phi, f(xdat,cffs), 'g--', label='fit')
-  file_name = "plot_set_number_{}_"+designation+".png".format(SetNum)
+  file_name = "plot_set_number_"+str(setNum)+"_gridMetric_"+designation+".png"
   plt.savefig(file_name)
     
 def cffs_from_globalModel(model, kinematics, numHL=1):
