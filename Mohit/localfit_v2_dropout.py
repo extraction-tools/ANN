7302f1fe32abb5dc6a71e9c2b2f4067d30a0755e
Mohit Srivastav <vir.moh@gmail.com>
2022-08-02 15:18:39 -0400

    dropout

--- Mohit/localfit_v2_dropout.py
+++ Mohit/localfit_v2_dropout.py
@@ -108,9 +108,9 @@ for epoch in np.arange(10, 1000, 50):  # parse the upper region less thoroughly
   for batch in np.arange(1, 47, 5):
     for i in np.arange(0, testnum, skip):
       for d_rate in np.arange(0, 0.61, 0.2):
+        
         for n, layer in enumerate(tfModel.layers):
           if 'dropout' in layer.name:
-            print(layer.rate)
             # changes the dropout rate for the model
             tfModel.layers[n].rate = d_rate
 
@@ -147,45 +147,44 @@ for epoch in np.arange(10, 1000, 50):  # parse the upper region less thoroughly
         total_rms_vals[selection_key] = total_rms
 
 
-for epoch in np.arange(1000, 7001, 500):  # parse the upper region less thoroughly
-  # 46 is greater than the 45 we need, but it will floor to 45
-  for batch in np.arange(1, 47, 5):
-    for i in np.arange(0, testnum, skip):
-      for d_rate in np.arange(0, 0.61, 0.2):
+# for epoch in np.arange(1000, 7001, 500):  # parse the upper region less thoroughly
+#   # 46 is greater than the 45 we need, but it will floor to 45
+#   for batch in np.arange(1, 47, 5):
+#     for i in np.arange(0, testnum, skip):
+#       for d_rate in np.arange(0, 0.61, 0.2):
 
-        for n, layer in enumerate(tfModel.layers):
-          if 'dropout' in layer.name:
-            print(layer.rate)
-            # changes the dropout rate for the model
-            tfModel.layers[n].rate = d_rate
+#         for n, layer in enumerate(tfModel.layers):
+#           if 'dropout' in layer.name:
+#             # changes the dropout rate for the model
+#             tfModel.layers[n].rate = d_rate
 
-        tfModel.compile(
-            optimizer=tf.keras.optimizers.Adam(.0085),
-            loss=tf.keras.losses.MeanSquaredError()
-        )
+#         tfModel.compile(
+#             optimizer=tf.keras.optimizers.Adam(.0085),
+#             loss=tf.keras.losses.MeanSquaredError()
+#         )
 
-        tfModel.set_weights(Wsave)  # resets the model
-        setI = data.getSet(i, itemsInSet=45)
+#         tfModel.set_weights(Wsave)  # resets the model
+#         setI = data.getSet(i, itemsInSet=45)
 
-        tfModel.fit([setI.Kinematics, setI.XnoCFF], setI.sampleY(),  # one replica of samples from F vals
-                    epochs=epoch, verbose=0, batch_size=batch, callbacks=[early_stopping_callback], validation_split=0.2)
+#         tfModel.fit([setI.Kinematics, setI.XnoCFF], setI.sampleY(),  # one replica of samples from F vals
+#                     epochs=epoch, verbose=0, batch_size=batch, callbacks=[early_stopping_callback], validation_split=0.2)
 
-        cffs = cffs_from_globalModel(tfModel, setI.Kinematics, numHL=4)
+#         cffs = cffs_from_globalModel(tfModel, setI.Kinematics, numHL=4)
 
-        new_xdat = np.transpose(setI.XnoCFF.to_numpy(dtype=np.float32))
+#         new_xdat = np.transpose(setI.XnoCFF.to_numpy(dtype=np.float32))
 
-        # Avoid recalculating F-values from cffs when that is what the model is predicting already
+#         # Avoid recalculating F-values from cffs when that is what the model is predicting already
 
-        F, total_error, max_residual, total_rms = F2VsPhi_noPlot(
-            df, i + 1, new_xdat, cffs
-        )  # runs the version without plotting to save time
+#         F, total_error, max_residual, total_rms = F2VsPhi_noPlot(
+#             df, i + 1, new_xdat, cffs
+#         )  # runs the version without plotting to save time
 
-        selection_key = (epoch, batch, d_rate, i)
-        F_vals[selection_key] = np.array(F)
-        cffs_record[selection_key] = np.array(cffs)
-        total_errors[selection_key] = total_error
-        total_residuals[selection_key] = max_residual
-        total_rms_vals[selection_key] = total_rms
+#         selection_key = (epoch, batch, d_rate, i)
+#         F_vals[selection_key] = np.array(F)
+#         cffs_record[selection_key] = np.array(cffs)
+#         total_errors[selection_key] = total_error
+#         total_residuals[selection_key] = max_residual
+#         total_rms_vals[selection_key] = total_rms
 
 base_cols = ["Epoch", "Batch", "Dropout Rate", "Set"]
 
@@ -217,88 +216,3 @@ while os.path.exists(final_str + '.csv'):
   filestr_num += 1
 
 total_metrics.to_csv(final_str + '.csv')  # ensures data is saved
-
-# best_combination_errors_df = pd.DataFrame.from_dict(best_combination_errors, orient="index", columns=['epoch', 'batch', 'rms'])
-# best_combination_errors_df.to_csv("best_combination_errors.csv")
-# best_combination_residual_df = pd.DataFrame.from_dict(best_combination_residual, orient="index", columns=['epoch', 'batch', 'rms'])
-# best_combination_residual_df.to_csv("best_combination_residual.csv")
-# best_combination_rms_df = pd.DataFrame.from_dict(best_combination_rms, orient="index", columns=['epoch', 'batch', 'rms'])
-# best_combination_rms_df.to_csv("best_combination_rms.csv")
-
-# most_common = []
-# for i,j,k in zip(best_combination_errors.values(), best_combination_residual.values(), best_combination_rms.values()):
-#   most_common.append(i[:2])
-#   most_common.append(j[:2])
-#   most_common.append(k[:2])
-
-# res_outcome = max(set(best_combination_residual.values()), key = list(best_combination_residual.values()).count)
-# print("Just using residuals, the best epoch number is:", res_outcome[0], "with a batch size of", res_outcome[1])
-
-# err_outcome = max(set(best_combination_errors.values()), key = list(best_combination_errors.values()).count)
-# print("Just using error, the best epoch number is:", err_outcome[0], "with a batch size of", err_outcome[1])
-
-# rms_outcome = max(set(best_combination_rms.values()), key = list(best_combination_rms.values()).count)
-# print("Just using root mean square error, the best epoch number is:", rms_outcome[0], "with a batch size of", rms_outcome[1])
-
-# final_outcome = max(set(most_common), key=most_common.count) #the final_outcome is a tuple of (epoch#, batch#)
-# print("Using all 3 metrics, the best epoch number is: ", final_outcome[0], "with a batch size of", final_outcome[1])
-
-
-# by_set = []
-# by_set_metrics = []
-
-# for i in range(0, testnum): #use the final outcome to have a final fit
-#   by_metric_fits = []
-#   for designator in ("err", "res", "rms", "final"):
-#     setI = data.getSet(i, itemsInSet=45)
-
-#     tfModel.set_weights(Wsave)
-#     tfModel.fit([setI.Kinematics, setI.XnoCFF], setI.sampleY(), # one replica of samples from F vals
-#                           epochs=eval(designator + "_outcome[0]"), verbose=0, batch_size=eval(designator + "_outcome[1]"), callbacks=[early_stopping_callback])
-
-#     cffs = cffs_from_globalModel(tfModel, setI.Kinematics, numHL=2)
-
-#     by_set.append([i, designator] + list(cffs))
-#     by_set_metrics.append([i, designator] + list(F2VsPhi_noPlot(df.copy(),i+1,new_xdat,cffs)))
-
-#     new_xdat = np.transpose(setI.XnoCFF.to_numpy(dtype=np.float32)) #NB: Could rewrite BHDVCS curve_fit to not require transposition
-
-#     # Avoid recalculating F-values from cffs when that is what the model is predicting already
-#     by_metric_fits.append(F2VsPhi_multiple_designations(df.copy(),i+1,new_xdat,cffs, designation = designator))
-
-#   for n, fit in enumerate(by_metric_fits): #iterate over the fits that were just produced
-#     if n == 0:
-#       plt.errorbar(*fit[1],fmt='.',color='blue',label="Data") #this is the "real" data
-
-#     plt.xlim(0,368)
-#     TempFvals = fit[1][1]
-#     temp_unit=(np.max(TempFvals)-np.min(TempFvals))/len(TempFvals)
-#     plt.ylim(np.min(TempFvals)-temp_unit,np.max(TempFvals)+temp_unit)
-
-#     grid_metric = ""
-#     if fit[0] == "err":
-#       grid_metric = "Mean Absolute Error"
-#     elif fit[0] == "res":
-#       grid_metric = "Maximum Residual"
-#     elif fit[0] == "rms":
-#       grid_metric = "NRMSE"
-#     else:
-#       grid_metric = "Most Common Hyperparameter Set"
-#     plt.plot(*fit[2], label='fit using ' + grid_metric, linestyle='dashed')
-
-#   plt.xticks(fontsize=15)
-#   plt.yticks(fontsize=15)
-#   plt.title("Local fits with data set #"+str(i),fontsize=20)
-#   plt.xlabel("phi")
-#   plt.ylabel("F")
-#   plt.legend(loc='best', fontsize=10,handlelength=3)
-#   file_name = "plot_set_number_"+str(i)+".png"
-#   plt.savefig(file_name)
-#   plt.clf()
-
-
-# newdf = pd.DataFrame(by_set)
-# newdf.to_csv('bySetCFFs.csv')
-
-# newdf2 = pd.DataFrame(by_set_metrics, columns=["set", "designator", "abs_err", "max_res", "rms_err"])
-# newdf2.to_csv("metrics.csv")
