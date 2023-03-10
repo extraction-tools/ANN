ec8f870bfd509b0f855e89be76ddc316b13d635f
lba9wf <51801672+lba9wf@users.noreply.github.com>
2023-03-10 17:40:39 -0500

    Add files via upload
    This is an example of the 6 files I used to run the code. I altered the localfit_v2.py code to localfit_1000files to make it run however many copies that i want. The 'CreateMeanSTDCSV' produces a mean and standard deviation file that are then both used by the 'ComparePlotFPlot graph.

---
+++ Librado/Comparing Re Values/BHDVCS_tf.py
@@ -0,0 +1,325 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+class TotalFLayer(tf.keras.layers.Layer):
+    def __init__(self):
+        super(TotalFLayer, self).__init__(dtype='float32')
+        self.f=BHDVCStf()
+        
+    def call(self, inputs):
+        return self.f.curve_fit(inputs[:, 0:7], inputs[:, 7:11])
+
+class BHDVCStf(object):
+
+    def __init__(self):
+        self.ALP_INV = tf.constant(137.0359998)  # 1 / Electromagnetic Fine Structure Constant
+        self.PI = tf.constant(3.1415926535)
+        self.RAD = tf.constant(self.PI / 180.)
+        self.M = tf.constant(0.938272)  # Mass of the proton in GeV
+        self.GeV2nb = tf.constant(.389379 * 1000000)  # Conversion from GeV to NanoBar
+        self.M2 = tf.constant(0.938272 * 0.938272)  # Mass of the proton  squared in GeV
+
+    @tf.function
+    def SetKinematics(self, QQ, x, t, k):
+        ee = 4. * self.M2 * x * x / QQ  # epsilon squared
+        y = tf.sqrt(QQ) / (tf.sqrt(ee) * k)  # lepton energy fraction
+        xi = x * (1. + t / 2. / QQ) / (2. - x + x * t / QQ);  # Generalized Bjorken variable
+        Gamma = x * y * y / self.ALP_INV / self.ALP_INV / self.ALP_INV / self.PI / 8. / QQ / QQ / tf.sqrt(1. + ee)  # factor in front of the cross section, eq. (22)
+        tmin = - QQ * (2. * (1. - x) * (1. - tf.sqrt(1. + ee)) + ee) / (4. * x * (1. - x) + ee)  # eq. (31)
+        Ktilde_10 = tf.sqrt(tmin - t) * tf.sqrt((1. - x) * tf.sqrt(1. + ee) + ((t - tmin) * (ee + 4. * x * (1. - x)) / 4. / QQ)) * tf.sqrt(1. - y - y * y * ee / 4.) / tf.sqrt(1. - y + y * y * ee / 4.)  # K tilde from 2010 paper
+        K = tf.sqrt(1. - y + y * y * ee / 4.) * Ktilde_10 / tf.sqrt(QQ)
+        return ee, y, xi, Gamma, tmin, Ktilde_10, K
+
+    @tf.function
+    def BHLeptonPropagators(self, phi, QQ, x, t, ee, y, K):
+        # KD 4-vector product (phi-dependent)
+        KD = - QQ / (2. * y * (1. + ee)) * (1. + 2. * K * tf.cos(self.PI - (phi * self.RAD)) - t / QQ * (1. - x * (2. - y) + y * ee / 2.) + y * ee / 2.)  # eq. (29)
+
+        # lepton BH propagators P1 and P2 (contaminating phi-dependence)
+        P1 = 1. + 2. * KD / QQ
+        P2 = t / QQ - 2. * KD / QQ
+        return P1, P2
+
+    @tf.function
+    def BHUU(self, phi, F1, F2, P1, P2, QQ, x, t, ee, y, Gamma, K):
+
+        # BH utorch.larized Fourier harmonics eqs. (35 - 37)
+
+        c0_BH = 8. * K * K * ((2. + 3. * ee) * (QQ / t) * (F1 * F1 - F2 * F2 * t / (4. * self.M2)) + 2. * x * x * (F1 + F2) * (F1 + F2)) + (2. - y) * (2. - y) * ((2. + ee) * (
+                    (4. * x * x * self.M2 / t) * (1. + t / QQ) * (
+                        1. + t / QQ) 
+                        + 4. * (1. - x) * (1. + x * t / QQ)) * (F1 * F1 - F2 * F2 * t / (4. * self.M2)) + 4. * x * x * (x + (1. - x + ee / 2.) * (1. - t / QQ) * (1. - t / QQ) - x * (1. - 2. * x) * t * t / (QQ * QQ)) * (F1 + F2) * (F1 + F2)) + 8. * (
+                                 1. + ee) * (1. - y - ee * y * y / 4.) * (
+                                 2. * ee * (1. - t / (4. * self.M2)) * (
+                                     F1 * F1 - F2 * F2 * t / (4. * self.M2)) - x * x * (
+                                             1. - t / QQ) * (1. - t / QQ) * (F1 + F2) * (F1 + F2))
+
+        c1_BH = 8. * K * (2. - y) * (
+                    (4. * x * x * self.M2 / t - 2. * x - ee) * (
+                        F1 * F1 - F2 * F2 * t / (4. * self.M2)) + 2. * x * x * (
+                                1. - (1. - 2. * x) * t / QQ) * (F1 + F2) * (F1 + F2))
+
+        c2_BH = 8. * x * x * K * K * (
+                    (4. * self.M2 / t) * (F1 * F1 - F2 * F2 * t / (4. * self.M2)) + 2. * (F1 + F2) * (
+                        F1 + F2))
+
+        # BH squared amplitude eq (25) divided by e^6
+        Amp2_BH = 1. / (x * x * y * y * (1. + ee) * (
+                    1. + ee) * t * P1 * P2) * (c0_BH + c1_BH * tf.cos(
+            self.PI - (phi * self.RAD)) + c2_BH * tf.cos(2. * (self.PI - (phi * self.RAD))))
+
+        Amp2_BH = self.GeV2nb * Amp2_BH  # convertion to nb
+
+        return Gamma * Amp2_BH
+
+    @tf.function
+    def IUU(self, phi, F1, F2, P1, P2, QQ, x, t, ee, y, K, Gamma, ReH, ReE, ReHtilde, twist, tmin, xi, Ktilde_10):
+
+        # Get BH propagators and set the kinematics
+        self.BHLeptonPropagators(phi, QQ, x, t, ee, y, K)
+
+        # Get A_UU_I, B_UU_I and C_UU_I interference coefficients
+        A_U_I, B_U_I, C_U_I = self.ABC_UU_I_10(phi, twist, QQ, x, t, ee, y, K, tmin, xi, Ktilde_10)
+
+        # BH-DVCS interference squared amplitude
+        I_10 = 1. / (x * y * y * y * t * P1 * P2) * (
+                    A_U_I * (F1 * ReH - t / 4. / self.M2 * F2 * ReE) + B_U_I * (F1 + F2) * (
+                        ReH + ReE) + C_U_I * (F1 + F2) * ReHtilde)
+
+        I_10 = self.GeV2nb * I_10  # convertion to nb
+
+        return Gamma * I_10
+
+    @tf.function
+    def ABC_UU_I_10(self, phi, twist, QQ, x, t, ee, y, K, tmin, xi, Ktilde_10):  # Get A_UU_I, B_UU_I and C_UU_I interference coefficients BKM10
+
+        if twist == "t2":
+            f = 0  # F_eff = 0 ( pure twist 2)
+        if twist == "t3":
+            f = - 2. * xi / (1. + xi)
+        if twist == "t3ww":
+            f = 2. / (1. + xi)
+
+        # Interference coefficients  (BKM10 Appendix A.1)
+        # n = 0 -----------------------------------------
+        # helicity - conserving (F)
+        C_110 = - 4. * (2. - y) * (1. + tf.sqrt(1 + ee)) / tf.pow((1. + ee), 2) * (
+                    Ktilde_10 * Ktilde_10 * (2. - y) * (2. - y) / QQ / tf.sqrt(1 + ee)
+                    + t / QQ * (1. - y - ee / 4. * y * y) * (2. - x) * (1. + (
+                        2. * x * (2. - x + (tf.sqrt(
+                    1. + ee) - 1.) / 2. + ee / 2. / x) * t / QQ + ee) / (2. - x) / (
+                                                                                                                       1. + tf.sqrt(
+                                                                                                                   1. + ee))))
+        C_110_V = 8. * (2. - y) / tf.pow((1. + ee), 2) * x * t / QQ * (
+                    (2. - y) * (2. - y) / tf.sqrt(1. + ee) * Ktilde_10 * Ktilde_10 / QQ
+                    + (1. - y - ee / 4. * y * y) * (1. + tf.sqrt(1. + ee)) / 2. * (
+                                1. + t / QQ) * (1. + (tf.sqrt(1. + ee) - 1. + 2. * x) / (
+                        1. + tf.sqrt(1. + ee)) * t / QQ))
+        C_110_A = 8. * (2. - y) / tf.pow((1. + ee), 2) * t / QQ * (
+                    (2. - y) * (2. - y) / tf.sqrt(
+                1. + ee) * Ktilde_10 * Ktilde_10 / QQ * (
+                                1. + tf.sqrt(1. + ee) - 2. * x) / 2.
+                    + (1. - y - ee / 4. * y * y) * ((1. + tf.sqrt(1. + ee)) / 2. * (
+                        1. + tf.sqrt(1. + ee) - x + (
+                            tf.sqrt(1. + ee) - 1. + x * (3. + tf.sqrt(1. + ee) - 2. * x) / (
+                                1. + tf.sqrt(1. + ee)))
+                        * t / QQ) - 2. * Ktilde_10 * Ktilde_10 / QQ))
+        # helicity - changing (F_eff)
+        C_010 = 12. * math.sqrt(2.) * K * (2. - y) * tf.sqrt(
+            1. - y - ee / 4. * y * y) / tf.pow(tf.sqrt(1. + ee), 5) * (
+                                 ee + (2. - 6. * x - ee) / 3. * t / QQ)
+        C_010_V = 24. * math.sqrt(2.) * K * (2. - y) * tf.sqrt(
+            1. - y - ee / 4. * y * y) / tf.pow(tf.sqrt(1. + ee),
+                                                                      5) * x * t / QQ * (
+                                   1. - (1. - 2. * x) * t / QQ)
+        C_010_A = 4. * math.sqrt(2.) * K * (2. - y) * tf.sqrt(
+            1. - y - ee / 4. * y * y) / tf.pow(tf.sqrt(1. + ee),
+                                                                      5) * t / QQ * (
+                                   8. - 6. * x + 5. * ee) * (
+                                   1. - t / QQ * ((2. - 12 * x * (1. - x) - ee)
+                                                            / (8. - 6. * x + 5. * ee)))
+        # n = 1 -----------------------------------------
+        # helicity - conserving (F)
+        C_111 = -16. * K * (1. - y - ee / 4. * y * y) / tf.pow(
+            tf.sqrt(1. + ee), 5) * ((1. + (1. - x) * (tf.sqrt(
+            1 + ee) - 1.) / 2. / x + ee / 4. / x) * x * t / QQ - 3. * ee / 4.) - 4. * K * (
+                                 2. - 2. * y + y * y + ee / 2. * y * y) * (
+                                 1. + tf.sqrt(1 + ee) - ee) / tf.pow(tf.sqrt(1. + ee), 5) * (
+                                 1. - (1. - 3. * x) * t / QQ + (
+                                     1. - tf.sqrt(1 + ee) + 3. * ee) / (
+                                             1. + tf.sqrt(1 + ee) - ee) * x * t / QQ)
+        C_111_V = 16. * K / tf.pow(tf.sqrt(1. + ee), 5) * x * t / QQ * (
+                    (2. - y) * (2. - y) * (1. - (1. - 2. * x) * t / QQ) + (
+                        1. - y - ee / 4. * y * y)
+                    * (1. + tf.sqrt(1. + ee) - 2. * x) / 2. * (t - tmin) / QQ)
+        C_111_A = -16. * K / tf.pow((1. + ee), 2) * t / QQ * (
+                    (1. - y - ee / 4. * y * y) * (1. - (1. - 2. * x) * t / QQ + (
+                        4. * x * (1. - x) + ee) / 4. / tf.sqrt(1. + ee) * (
+                                                                                  t - tmin) / QQ)
+                    - tf.pow((2. - y), 2) * (
+                                1. - x / 2. + (1. + tf.sqrt(1. + ee) - 2. * x) / 4. * (
+                                    1. - t / QQ) + (4. * x * (1. - x) + ee) / 2. / tf.sqrt(
+                            1. + ee) * (t - tmin) / QQ))
+        # helicity - changing (F_eff)
+        C_011 = 8. * math.sqrt(2.) * tf.sqrt(1. - y - ee / 4. * y * y) / tf.pow(
+            (1. + ee), 2) * (tf.pow((2. - y), 2) * (t - tmin) / QQ * (
+                    1. - x + ((1. - x) * x + ee / 4.) / tf.sqrt(1. + ee) * (
+                        t - tmin) / QQ)
+                                  + (1. - y - ee / 4. * y * y) / tf.sqrt(1 + ee) * (
+                                              1. - (1. - 2. * x) * t / QQ) * (
+                                              ee - 2. * (1. + ee / 2. / x) * x * t / QQ))
+        C_011_V = 16. * math.sqrt(2.) * tf.sqrt(1. - y - ee / 4. * y * y) / tf.pow(
+            tf.sqrt(1. + ee), 5) * x * t / QQ * (
+                                   tf.pow(Ktilde_10 * (2. - y), 2) / QQ + tf.pow(
+                               (1. - (1. - 2. * x) * t / QQ), 2) * (
+                                               1. - y - ee / 4. * y * y))
+        C_011_A = 8. * math.sqrt(2.) * tf.sqrt(1. - y - ee / 4. * y * y) / tf.pow(
+            tf.sqrt(1. + ee), 5) * t / QQ * (
+                                   tf.pow(Ktilde_10 * (2. - y), 2) * (1. - 2. * x) / QQ + (
+                                       1. - (1. - 2. * x) * t / QQ)
+                                   * (1. - y - ee / 4. * y * y) * (
+                                               4. - 2. * x + 3. * ee + t / QQ * (
+                                                   4. * x * (1. - x) + ee)))
+        # n = 2 -----------------------------------------
+        # helicity - conserving (F)
+        C_112 = 8. * (2. - y) * (1. - y - ee / 4. * y * y) / tf.pow((1. + ee),
+                                                                                                     2) * (
+                                 2. * ee / tf.sqrt(1. + ee) / (1. + tf.sqrt(1. + ee)) * tf.pow(
+                             Ktilde_10, 2) / QQ + x * t * (
+                                             t - tmin) / QQ / QQ * (1. - x - (
+                                     tf.sqrt(1. + ee) - 1.) / 2. + ee / 2. / x))
+        C_112_V = 8. * (2. - y) * (1. - y - ee / 4. * y * y) / tf.pow((1. + ee),
+                                                                                                       2) * x * t / QQ * (
+                                   4. * tf.pow(Ktilde_10, 2) / tf.sqrt(1. + ee) / QQ + (
+                                       1. + tf.sqrt(1. + ee) - 2. * x) / 2. * (1. + t / QQ) * (
+                                               t - tmin) / QQ)
+        C_112_A = 4. * (2. - y) * (1. - y - ee / 4. * y * y) / tf.pow((1. + ee),
+                                                                                                       2) * t / QQ * (
+                                   4. * (1. - 2. * x) * tf.pow(Ktilde_10, 2) / tf.sqrt(
+                               1. + ee) / QQ - (3. - tf.sqrt(
+                               1. + ee) - 2. * x + ee / x) * x * (
+                                               t - tmin) / QQ)
+        # helicity - changing (F_eff)
+        C_012 = -8. * math.sqrt(2.) * K * (2. - y) * tf.sqrt(
+            1. - y - ee / 4. * y * y) / tf.pow(tf.sqrt(1. + ee), 5) * (
+                                 1. + ee / 2.) * (
+                                 1. + (1. + ee / 2. / x) / (1. + ee / 2.) * x * t / QQ)
+        C_012_V = 8. * math.sqrt(2.) * K * (2. - y) * tf.sqrt(
+            1. - y - ee / 4. * y * y) / tf.pow(tf.sqrt(1. + ee),
+                                                                      5) * x * t / QQ * (
+                                   1. - (1. - 2. * x) * t / QQ)
+        C_012_A = 8. * math.sqrt(2.) * K * (2. - y) * tf.sqrt(
+            1. - y - ee / 4. * y * y) / tf.pow((1. + ee), 2) * t / QQ * (
+                                   1. - x + (t - tmin) / 2. / QQ * (
+                                       4. * x * (1. - x) + ee) / tf.sqrt(1. + ee))
+        # n = 3 -----------------------------------------
+        # helicity - conserving (F)
+        C_113 = -8. * K * (1. - y - ee / 4. * y * y) / tf.pow(tf.sqrt(1. + ee),
+                                                                                               5) * (
+                                 tf.sqrt(1. + ee) - 1.) * (
+                                 (1. - x) * t / QQ + (tf.sqrt(1. + ee) - 1.) / 2. * (
+                                     1. + t / QQ))
+        C_113_V = -8. * K * (1. - y - ee / 4. * y * y) / tf.pow(
+            tf.sqrt(1. + ee), 5) * x * t / QQ * (tf.sqrt(1. + ee) - 1. + (
+                    1. + tf.sqrt(1. + ee) - 2. * x) * t / QQ)
+        C_113_A = 16. * K * (1. - y - ee / 4. * y * y) / tf.pow(
+            tf.sqrt(1. + ee), 5) * t * (t - tmin) / QQ / QQ * (
+                                   x * (1. - x) + ee / 4.)
+
+        # A_U_I, B_U_I and C_U_I
+        A_U_I = C_110 + math.sqrt(2.) / (2. - x) * Ktilde_10 / tf.sqrt(
+            QQ) * f * C_010 + (C_111 + math.sqrt(2.) / (2. - x) * Ktilde_10 / tf.sqrt(
+            QQ) * f * C_011) * tf.cos(self.PI - (phi * self.RAD)) + (
+                                 C_112 + math.sqrt(2.) / (2. - x) * Ktilde_10 / tf.sqrt(
+                             QQ) * f * C_012) * tf.cos(
+            2. * (self.PI - (phi * self.RAD))) + C_113 * tf.cos(3. * (self.PI - (phi * self.RAD)))
+        B_U_I = xi / (1. + t / 2. / QQ) * (
+                    C_110_V + math.sqrt(2.) / (2. - x) * Ktilde_10 / tf.sqrt(
+                QQ) * f * C_010_V + (
+                                C_111_V + math.sqrt(2.) / (2. - x) * Ktilde_10 / tf.sqrt(
+                            QQ) * f * C_011_V) * tf.cos(self.PI - (phi * self.RAD)) + (
+                                C_112_V + math.sqrt(2.) / (2. - x) * Ktilde_10 / tf.sqrt(
+                            QQ) * f * C_012_V) * tf.cos(
+                2. * (self.PI - (phi * self.RAD))) + C_113_V * tf.cos(3. * (self.PI - (phi * self.RAD))))
+        C_U_I = xi / (1. + t / 2. / QQ) * (
+                    C_110 + C_110_A + math.sqrt(2.) / (2. - x) * Ktilde_10 / tf.sqrt(
+                QQ) * f * (C_010 + C_010_A) + (
+                                C_111 + C_111_A + math.sqrt(2.) / (2. - x) * Ktilde_10 / tf.sqrt(
+                            QQ) * f * (C_011 + C_011_A)) * tf.cos(self.PI - (phi * self.RAD)) + (
+                                C_112 + C_112_A + math.sqrt(2.) / (2. - x) * Ktilde_10 / tf.sqrt(
+                            QQ) * f * (C_012 + C_012_A)) * tf.cos(
+                2. * (self.PI - (phi * self.RAD))) + (C_113 + C_113_A) * tf.cos(
+                3. * (self.PI - (phi * self.RAD))))
+        
+        return A_U_I, B_U_I, C_U_I
+                
+    @tf.function
+    def curve_fit(self, kins, cffs):
+        phi, QQ, x, t, k, F1, F2 = tf.split(kins, num_or_size_splits=7, axis=1)
+        ReH, ReE, ReHtilde, c0fit = tf.split(cffs, num_or_size_splits=4, axis=1)  # output of network
+        ee, y, xi, Gamma, tmin, Ktilde_10, K = self.SetKinematics(QQ, x, t, k)
+        P1, P2 = self.BHLeptonPropagators(phi, QQ, x, t, ee, y, K)
+        xsbhuu = self.BHUU(phi, F1, F2, P1, P2, QQ, x, t, ee, y, Gamma, K)
+        xsiuu = self.IUU(phi, F1, F2, P1, P2, QQ, x, t, ee, y, K, Gamma, ReH, ReE, ReHtilde, "t2", tmin, xi, Ktilde_10)
+        f_pred = xsbhuu + xsiuu + c0fit
+        return f_pred
+        
+class DvcsData(object):
+    def __init__(self, df):
+      self.df = df
+      self.X = df.loc[:, ['phi_x', 'k', 'QQ', 'x_b', 't', 'F1', 'F2', 'dvcs']]
+      self.XnoCFF = df.loc[:, ['phi_x', 'QQ', 'x_b', 't', 'k', 'F1', 'F2']]               
+      self.y = df.loc[:, 'F']
+      self.Kinematics = df.loc[:, ['k', 'QQ', 'x_b', 't']]
+      self.erry = df.loc[:, 'errF']
+        
+    def __len__(self):
+      return len(self.X)
+    
+    def getSet(self, setNum, itemsInSet=36):
+      pd.options.mode.chained_assignment = None
+      subX = self.X.loc[setNum*itemsInSet:(setNum+1)*itemsInSet-1, :]
+      subX['F'] = self.y.loc[setNum*itemsInSet:(setNum+1)*itemsInSet-1]
+      subX['errF'] = self.erry.loc[setNum*itemsInSet:(setNum+1)*itemsInSet-1]
+      pd.options.mode.chained_assignment = 'warn'
+      return DvcsData(subX)
+    
+    def sampleY(self):
+      return np.random.normal(self.y, self.erry)
+    
+    def sampleWeights(self):
+      return 1/self.erry
+    
+    def getAllKins(self, itemsInSets=36):
+      return self.Kinematics.iloc[np.array(range(len(df)//itemsInSets))*itemsInSets, :]
+
+def F2VsPhi(dataframe,SetNum,xdat,cffs):
+  f = BHDVCStf().curve_fit
+  TempFvalSilces=dataframe[dataframe["#Set"]==SetNum]
+  TempFvals=TempFvalSilces["F"]
+  TempFvals_sigma=TempFvalSilces["errF"]
+
+  temp_phi=TempFvalSilces["phi_x"]
+  plt.errorbar(temp_phi,TempFvals,TempFvals_sigma,fmt='.',color='blue',label="Data")
+  plt.xlim(0,368)
+  temp_unit=(np.max(TempFvals)-np.min(TempFvals))/len(TempFvals)
+  plt.ylim(np.min(TempFvals)-temp_unit,np.max(TempFvals)+temp_unit)
+  plt.xticks(fontsize=15)
+  plt.yticks(fontsize=15)
+  plt.legend(loc=4,fontsize=10,handlelength=3)
+  plt.title("Local fit with data set #"+str(SetNum),fontsize=20)
+  plt.plot(temp_phi, f(xdat,cffs), 'g--', label='fit')
+  file_name = "plot_set_number_{}.png".format(SetNum)
+  plt.savefig(file_name)
+    
+def cffs_from_globalModel(model, kinematics, numHL=1):
+  '''
+  :param model: the model from which the cffs should be predicted
+  :param kinematics: the kinematics that should be used to predict
+  :param numHL: the number of hidden layers:
+  '''
+  subModel = tf.keras.backend.function(model.layers[0].input, model.layers[numHL+2].output)
+  return subModel(np.asarray(kinematics)[None, 0])[0]
