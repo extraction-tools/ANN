e66375ed5f61abd977216cee9b7d2788448a54f4
librado <lba9wf@virginia.edu>
2023-04-24 15:45:48 -0500

    Updated code to be faster and output graph without comparing

--- Baseline/BKM10-tf/Baseline_ComparisonPlot.py
+++ Baseline/BKM10-tf/Baseline_ComparisonPlot.py
@@ -26,72 +26,79 @@ class ComparisonPlot:
         self.true_value_path = true_value_path if true_value_path is not None else "pseudo_KM15.csv"
         
     def plot(self):
-        
-        # Find all the files in the folder_path that start with "bySetCFFs"
-        files = [f for f in os.listdir(self.folder_path) if f.startswith("bySetCFFs")]
-
-        # Read the CSV files into a list of DataFrames
-        dfs = [pd.read_csv(os.path.join(self.folder_path, f), index_col=0) for f in files]
-
-        # Count the number of files read through the folder
-        num_files = len(dfs)
-
-        # Concatenate the DataFrames along the third dimension
-        array_3d = np.stack([df.values for df in dfs], axis=-1)
-
-        # Compute the mean and standard deviation along the third dimension
-        array_mean = np.mean(array_3d, axis=-1)
-        array_stdev = np.std(array_3d, axis=-1)
-
-        # Convert the resulting NumPy arrays into DataFrames and set the column and index names
-        df_mean = pd.DataFrame(array_mean, columns=dfs[0].columns, index=dfs[0].index)
-        df_stdev = pd.DataFrame(array_stdev, columns=dfs[0].columns, index=dfs[0].index)
-
-        # Shift the index of both DataFrames by 1
-        df_mean.index = df_mean.index + 1
-        df_stdev.index = df_stdev.index + 1
-        
-        # Convert the mean and stdev arrays to DataFrames and set the column and index names
-        df_mean.columns = ['ReH', 'ReE', 'ReHTilde', 'dvcs']
-        df_stdev.columns = ['ReH', 'ReE', 'ReHTilde', 'dvcs']
-        
-        #plot x range default
-        if self.x_range is None:
-            self.x_range = [1,np.shape(df_mean)[0]]
+        if self.folder_path is not None:
+            # Find all the files in the folder_path that start with "bySetCFFs"
+            files = [f for f in os.listdir(self.folder_path) if f.startswith("bySetCFFs")]
+            # Read the CSV files into a list of DataFrames
+            dfs = [pd.read_csv(os.path.join(self.folder_path, f), index_col=0) for f in files]
+            
+            # Count the number of files read through the folder
+            num_files = len(dfs)
+            
+            # Concatenate the DataFrames along the third dimension
+            array_3d = np.stack([df.values for df in dfs], axis=-1)
+            # Compute the mean and standard deviation along the third dimension
+            array_mean = np.mean(array_3d, axis=-1)
+            array_stdev = np.std(array_3d, axis=-1)
+            
+            # Convert the resulting NumPy arrays into DataFrames and set the column and index names
+            df_mean = pd.DataFrame(array_mean, columns=dfs[0].columns, index=dfs[0].index)
+            df_stdev = pd.DataFrame(array_stdev, columns=dfs[0].columns, index=dfs[0].index)
+            
+            # Shift the index of both DataFrames by 1
+            df_mean.index = df_mean.index + 1
+            df_stdev.index = df_stdev.index + 1
+            
+            # Convert the mean and stdev arrays to DataFrames and set the column and index names
+            df_mean.columns = ['ReH', 'ReE', 'ReHTilde', 'dvcs']
+            df_stdev.columns = ['ReH', 'ReE', 'ReHTilde', 'dvcs']
+            
+            #plot x range default
+            if self.x_range is None:
+                self.x_range = [1,np.shape(df_mean)[0]]
+        else:
+            result = pd.read_csv(self.true_value_path)
+            self.x_range = [1,result.iloc[-1,0]]
 
         # Filter the data based on x range
         result = pd.read_csv(self.true_value_path)
-        result_filtered = result[(result['#Set'] >= self.x_range[0]) & (result['#Set'] <= self.x_range[1])]
-        df_filtered = df_mean[(df_mean.index >= self.x_range[0]) & (df_mean.index <= self.x_range[1])].reset_index()
-        y_err_filtered = df_stdev[(df_stdev.index >= self.x_range[0]) & (df_stdev.index <= self.x_range[1])][self.cff].to_numpy()
+        result_filtered = pd.read_csv(self.true_value_path)[(result['#Set'] >= self.x_range[0]) & (result['#Set'] <= self.x_range[1])]
+        if self.folder_path is not None:
+            df_filtered = df_mean[(df_mean.index >= self.x_range[0]) & (df_mean.index <= self.x_range[1])].reset_index()
+            y_err_filtered = df_stdev[(df_stdev.index >= self.x_range[0]) & (df_stdev.index <= self.x_range[1])][self.cff].to_numpy()
 
         # Plot filtered mean and filtered errorbars
-        if self.show_mean_points:
+        if self.folder_path is not None and self.show_mean_points:
             plt.scatter(np.array(df_filtered.iloc[:, 0]), np.array(df_filtered[self.cff]), color=self.fit_color, s=self.point_size)
-        if self.show_error_bars:
+        if self.folder_path is not None and self.show_error_bars:
             plt.errorbar(np.array(df_filtered.iloc[:, 0]), np.array(df_filtered[self.cff]), color=self.fit_color, yerr=y_err_filtered, fmt='none', capsize=3, label = 'Fitted Values')
 
         # Plot x against y
-        plt.scatter(result_filtered['#Set'], result_filtered[self.cff], color=self.true_color, s=self.point_size, label = 'True Values')
+        plt.scatter(result_filtered['#Set'], result_filtered[self.cff], color=self.true_color, s=self.point_size, label='True Values')
 
         # Modify the plot title based on the number of files and column_y
-        plt.title(f'{self.cff} Comparison Plot for {num_files} Replicas (range:{self.x_range})')
+        if self.folder_path is not None:
+            plt.title(f'{self.cff} Comparison Plot for {num_files} Replicas (range:{self.x_range})')
+        else:
+            plt.title(f'{self.cff} Comparison Plot (range:{self.x_range})')
         plt.legend()
         plt.xlabel('Bin')
         plt.ylabel(f'{self.cff}')
-        
-        #Filter the plot based on y range
+
+        # Filter the plot based on y range
         if self.y_range is not None:
             plt.ylim(self.y_range)
-        
-        #Saves plot to path and sets default name of file
+
+        # Saves plot to path and sets default name of file
         if self.save_path is not None:
-            self.file_name = f"{self.file_name}_{self.cff}_{num_files}_replicas_range_{self.x_range}"
+            self.file_name = f"{self.file_name}_{self.cff}"
+            if self.folder_path is not None:
+                self.file_name += f"_{num_files}_replicas_range_{self.x_range}"
+            else:
+                self.file_name += f"_replicas_range_{self.x_range}"
             plt.savefig(os.path.join(self.save_path, self.file_name))
-                    
-                    
-            
-        #Show plot
+
+        # Show plot
         if self.show_plot:
             plt.show()
 
